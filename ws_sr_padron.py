2c78c27abd6966ceadc1cec481d32fd5522c6054
Alejandro Naifuino <alenaifuino@gmail.com>
2017-12-31 02:28:43 -0300

    Mejoro implementación del método get_census_data y lo renombro a get_scope_data

--- ws_sr_padron.py
+++ ws_sr_padron.py
@@ -38,8 +38,6 @@ WS_SR_PADRON_A100 - Especificación Técnica v1.1 en:
 http://www.afip.gov.ar/ws/ws_sr_padron_a100/manual_ws_sr_padron_a100_v1.1.pdf
 """
 
-import logging
-import sys
 from datetime import datetime
 from json import dumps
 
@@ -49,10 +47,10 @@ from wsaa import WSAA
 __author__ = 'Alejandro Naifuino (alenaifuino@gmail.com)'
 __copyright__ = 'Copyright (C) 2017 Alejandro Naifuino'
 __license__ = 'GPL 3.0'
-__version__ = '0.8.12'
+__version__ = '0.9.2'
 
 
-class WSSRPADRON(web_service.BaseWebService):
+class WSSRPADRON(web_service.WSBAse):
     """
     Clase que se usa de interfaz para el Web Service de Consulta a Padrón AFIP:
         - Alcance 4
@@ -60,47 +58,55 @@ class WSSRPADRON(web_service.BaseWebService):
         - Alcance 10
         - Alcance 100
     """
-    def __init__(self, config):
-        self.config = config
-        super().__init__(self.config, '<string>.json')
 
-    def get_census_data(self, option, ticket_data):
+    def __init__(self, config):
+        super().__init__(config['debug'], config['ws_wsdl'],
+                         config['web_service'], '<string>.json')
+        self.cuit = config['cuit']
+        self.scope = config['scope']
+        self.option = config['option']
+        self.path = None
+
+    def get_scope_data(self):
         """
         Método genérico que obtiene el método solicitado en option
         """
-        # Valido que el servicio de AFIP este funcionando
+        # Valido que el servicio de AFIP esté funcionando
         if self.dummy():
             raise SystemExit('El servicio de AFIP no se encuentra disponible')
 
-        # Defino los parámetros comunes de los web services padron de AFIP
+        # Establezco el lugar donde se guardarán los datos
+        self.path = self.get_output_path(name=self.option)
+
+        # Defino los parámetros comunes para los Web Services padrón de AFIP
         params = {
-            'token': ticket_data['token'],
-            'sign': ticket_data['sign'],
-            'cuitRepresentada': self.config['cuit']
+            'token': self.token,
+            'sign': self.sign,
+            'cuitRepresentada': self.cuit
         }
 
-        # Defino los parámetros y el método según la opción suministrada
-        if option == 'taxpayer':
-            params['idPersona'] = self.config[option]
-            service_name = 'getPersona'
-        elif option == 'table':
-            params['collectionName'] = self.config[option]
-            service_name = 'getParameterCollectionByName'
+        # Defino el método dependiendo del alcance seleccionado
+        if self.scope != 100:
+            method = 'getPersona'
+            params.update({'idPersona': self.option})
+        else:
+            method = 'getParameterCollectionByName'
+            params.update = ({'collectionName': self.option})
 
         # Obtengo la respuesta del WSDL de AFIP
-        response = self.soap_connect(
-            self.config['ws_wsdl'], service_name, params)
+        response = self.soap_connect(self.ws_wsdl, method, params)
 
-        # Recorro y modifico el diccionario para los items del tipo datetime
-        utility.map_nested_dicts(response, utility.datetime_to_string, datetime)
+        # Recorro el diccionario de respuesta y convierto los items del tipo
+        # datetime a string
+        utility.map_nested_dicts(response, utility.datetime_to_string,
+                                 datetime)
 
         # Lo transformo a JSON
         json_response = dumps(response, indent=2, ensure_ascii=False)
 
         # Genero el archivo con la respuesta de AFIP
-        output = self.get_output_path(name=self.config[option])
-        with open(output, 'w') as file:
-            file.write(json_response)
+        with open(self.path, 'w') as _:
+            _.write(json_response)
 
         return json_response
 
@@ -112,6 +118,9 @@ def main():
     # Obtengo los parámetros pasados por línea de comandos
     args = utility.cli_parser(__file__, __version__)
 
+    # Establezco el nombre del web service según el alcance
+    args['web_service'] = 'ws_sr_padron_a' + str(args['scope'])
+
     # Obtengo los datos de configuración
     try:
         config_data = utility.get_config_data(args)
@@ -120,35 +129,22 @@ def main():
 
     # Muestro las opciones de configuración via stdout
     if config_data['debug']:
-        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-        logging.info('|============  Configuración  ============')
-        logging.info('| Certificado:   %s', config_data['certificate'])
-        logging.info('| Clave Privada: %s', config_data['private_key'])
-        logging.info('| Frase Secreta: %s',
-                     '******' if config_data['passphrase'] else None)
-        logging.info('| WSAA WSDL:     %s', config_data['wsdl'])
-        logging.info('| WS:            %s', config_data['web_service'])
-        logging.info('| WS WSDL:       %s', config_data['ws_wsdl'])
-        logging.info('|=================  ---  =================')
-
-    # Instancio WSSRPADRON para obtener un objeto de padrón AFIP
-    census = WSSRPADRON(config_data)
+        utility.print_config(config_data)
 
     # Instancio WSAA para obtener un objeto de autenticación y autorización
     wsaa = WSAA(config_data)
 
-    # Obtengo el ticket de autorización de AFIP
-    ticket_data = wsaa.get_ticket()
+    # Instancio WSSRPADRON para obtener un objeto de padrón AFIP
+    census = WSSRPADRON(config_data)
 
-    # Defino el método de conexión
-    option = 'table' if config_data['scope'] == 100 else 'taxpayer'
+    # Obtengo el ticket de autorización de AFIP
+    census.token, census.sign = wsaa.get_ticket()
 
     # Obtengo los datos solicitados
-    census.get_census_data(option, ticket_data)
+    census.get_scope_data()
 
     # Imprimo la ubicación del archivo de salida
-    print('Respuesta AFIP en: {}'.format(
-        census.get_output_path(name=config_data[option])))
+    print('Respuesta AFIP en: {}'.format(census.path))
 
 
 if __name__ == '__main__':
