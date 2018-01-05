f377aa8197052106376ed3a33007f00961726687
Alejandro Naifuino <alenaifuino@gmail.com>
2018-01-05 01:52:16 -0300

    Reimplemento las funciones check_cuit para que haga raise del error en lugar de devolver true o false. Modifico los mensajes de error de check_file. Rediseño check_config y check_cli

--- libs/validation.py
+++ libs/validation.py
@@ -16,52 +16,50 @@ Módulo con funciones auxiliares para la gestión de validación de input
 __author__ = "Alejandro Naifuino <alenaifuino@gmail.com>"
 __copyright__ = "Copyright (C) 2017 Alejandro Naifuino"
 __license__ = "GPL 3.0"
-__version__ = "0.6.4"
+__version__ = "0.7.9"
 
 
 def check_cuit(cuit):
     """
     Valida la Clave Unica de Identificación Tributaria (CUIT).
     Formato válido: xxyyyyyyyyz o xx-yyyyyyyy-z
-
-    Devuelve False si el CUIT no es válido
     """
     # Valido la longitud del cuit
     if not cuit:
-        return False
-    elif len(cuit) != 11:
-        return False
+        raise ValueError('La CUIT suministrada está vacía')
     elif len(cuit) == 13:
-        cuit = cuit.replace('-', '', 2)
+        cuit = cuit[:2] + cuit[3:11] + cuit[:-1]
+    elif len(cuit) != 11:
+        raise ValueError('La CUIT suministrada no es válida')
 
     # Verificación en Base10
     base = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
 
     # Calculo el dígito verificador
-    verificador = 0
-    for i in range(10):
-        verificador += int(cuit[i]) * base[i]
-    verificador = 11 - (verificador - (int(verificador / 11) * 11))
+    list_sum = sum([int(cuit[i]) * base[i] for i in range(10)])
+    checker = 11 - (list_sum - ((list_sum // 11) * 11))
 
-    if verificador == 11:
-        verificador = 0
-    elif verificador == 10:
-        verificador = 9
+    if checker == 11:
+        checker = 0
+    elif checker == 10:
+        checker = 9
 
-    return verificador == int(cuit[10])
+    if not str(checker) == cuit[10]:
+        raise ValueError('La CUIT suministrada no es válida')
+
+    return True
 
 
-def check_file(file, *, name, permission='r'):
+def check_file(file, permission='r'):
     """
     Valida que un archivo exista y que tenga los permisos requeridos
     """
     try:
         open(file, permission)
     except FileNotFoundError:
-        raise ValueError('{}: no se encontró el archivo'.format(name))
+        raise ValueError('No se encontró el archivo solicitado')
     except PermissionError:
-        raise ValueError(
-            '{}: no tiene los permisos requeridos'.format(name))
+        raise ValueError('El archivo no tiene los permisos requeridos')
 
     return True
 
@@ -72,27 +70,72 @@ def check_config(data):
     """
     from urllib.parse import urlparse
 
-    # Valida el CUIT
-    if not check_cuit(data['cuit']):
-        raise ValueError('La clave "cuit" no es válida')
+    # Valida el DN
+    if not isinstance(data['dn'], str):
+        raise ValueError('Error de configuración en [dn]: no es válido')
 
-    # Valida el Certificado
-    check_file(data['certificate'], name='certificate')
-
-    # Valida la Clave Privada
-    check_file(data['private_key'], name='private_key')
-
-    # Valida la Frase Secreta
-    if data['passphrase']:
-        if not isinstance(data['passphrase'], str):
-            raise ValueError('La clave "passphrase" no es una cadena de texto')
+    # Valida el certificado y la clave privada
+    for value in ['certificate', 'private_key']:
+        try:
+            check_file(data[value])
+        except ValueError as error:
+            raise ValueError('Error de configuración en [{}]: {}'.format(
+                value,
+                str(error).lower()))
 
     # Valida los WSDL
-    for wsdl in ['wsdl', 'ws_wsdl']:
-        parsed = urlparse(data[wsdl])
+    for value in ['wsdl', 'ws_wsdl']:
+        parsed = urlparse(data[value])
         if not parsed.scheme or not parsed.netloc:
-            raise ValueError('La clave "{}" no es válida'.format(wsdl))
+            raise ValueError('Error de configuración en [{}]: no es una URL '
+                             'válida'.format(value))
 
-    # Valida debug
-    if not isinstance(data['debug'], bool):
-        raise ValueError('El modo debug no es válido')
+    # Valida el modo prod y debug
+    for value in ['prod', 'debug']:
+        if not isinstance(data[value], bool):
+            raise ValueError('Error de configuración en [{}]: el modo no es '
+                             'válido'.format(value))
+
+
+def check_cli(parser, **kwargs):
+    """
+    Wrapper que valida los argumentos suministrados por la línea de comandos
+    """
+    if kwargs['type'] == 'cuit':
+        try:
+            check_cuit(kwargs['value'])
+        except ValueError as message:
+            raise parser.error(message)
+    elif kwargs['type'] == 'file':
+        try:
+            check_file(kwargs['value'])
+        except ValueError as message:
+            raise parser.error(kwargs['arg'] + ': ' + message)
+    elif kwargs['type'] == 'str':
+        if not isinstance(kwargs['value'], str):
+            raise parser.error(kwargs['arg'] + ': no es una cadena de texto')
+    elif kwargs['type'] == 'list':
+        if kwargs['value'] not in kwargs['list']:
+            raise parser.error(kwargs['arg'] + ': no es un valor válido. '
+                               'Valores válidos: ' + ', '.join(kwargs['list']))
+
+    return kwargs['value']
+    '''
+    if script == 'ws_sr_padron.py':
+        if args.scope == 100:
+            if not args.option:
+                raise ValueError('Debe definir la tabla a consultar')
+            elif args.option not in A100_COLLECTIONS:
+                raise ValueError('La tabla suministrada no es válida')
+        #elif args.scope != 100:
+        #    if not args.option:
+        #        raise ValueError('La opción --persona debe definir el CUIT '
+        #                         'del contribuyente a consultar en el '
+        #                         'Padrón AFIP')
+    elif script == 'wsfe.py':
+        if not args.type or not args.parameter:
+            raise ValueError('Debe seleccionar un comprobante a autorizar o '
+                             'un parámetro a consultar')
+        if args.parameter == 'cotizacion' and not args.currency_id:
+            raise ValueError('Debe definir el ID de la moneda a cotizar')
+    '''
